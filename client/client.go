21fef2828b7ccaa91c4565a82d33d0bdfeb17eaa
swanandx <73115739+swanandx@users.noreply.github.com>
2023-12-31 19:41:04 +0530

    ch04: protocol parsing

--- client/client.go
+++ client/client.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
 	"net"
 )
@@ -10,24 +12,104 @@ func main() {
 	conn, err := net.Dial("tcp", ":1234")
 	if err != nil {
 		log.Fatal("Failed to connect: ", err)
-		return
 	}
 
 	defer conn.Close()
 
-	msg := []byte("hello")
-	_, err = conn.Write(msg)
+	err = query(conn, "hello1")
 	if err != nil {
-		log.Fatal("Failed to write: ", err)
-		return
+		log.Fatal("query failed: ", err)
 	}
 
-	rbuf := make([]byte, 64)
-	_, err = conn.Read(rbuf)
+	err = query(conn, "hello2")
 	if err != nil {
-		log.Fatal("Failed to read: ", err)
-		return
+		log.Fatal("query failed: ", err)
 	}
 
-	fmt.Println("server says: ", string(rbuf))
+	err = query(conn, "hello3")
+	if err != nil {
+		log.Fatal("query failed: ", err)
+	}
+}
+
+var k_max_msg uint32 = 4096
+
+func query(conn net.Conn, text string) error {
+	msg_len := uint32(len(text))
+	if msg_len > k_max_msg {
+		return fmt.Errorf("Message too long")
+	}
+
+	wbuf := make([]byte, 4+k_max_msg)
+
+	binary.LittleEndian.PutUint32(wbuf, msg_len)
+	copy(wbuf[4:], text)
+	err := write_all(conn, wbuf, int(4+msg_len))
+
+	if err != nil {
+		return err
+	}
+
+	rbuf := make([]byte, 4+k_max_msg+1)
+	err = read_full(conn, rbuf, 4)
+	if err != nil {
+		return err
+	}
+
+	msg_len = binary.LittleEndian.Uint32(rbuf[:4])
+
+	if msg_len > k_max_msg {
+		return fmt.Errorf("Message too long!")
+	}
+
+	// reply body
+	err = read_full(conn, rbuf[4:], int(msg_len))
+	if err != nil {
+		return err
+	}
+
+	// do something
+	rbuf[4+msg_len] = '\x00'
+
+	fmt.Println("server says: ", string(rbuf[4:]))
+
+	return nil
+}
+
+func read_full(c net.Conn, buf []byte, n int) error {
+	reader := io.LimitReader(c, int64(n))
+	pos := 0
+	for n > 0 {
+		rv, err := reader.Read(buf[pos:])
+		if err != nil {
+			return err
+		}
+
+		if rv > n {
+			panic("we have limited the reader, we should never reach here")
+		}
+
+		n -= rv
+		pos += rv
+	}
+
+	return nil
+}
+
+func write_all(c net.Conn, buf []byte, n int) error {
+	pos := 0
+	for n > 0 {
+		rv, err := c.Write(buf[pos:n])
+		if err != nil {
+			return err
+		}
+
+		if rv > n {
+			panic("should never happen right?")
+		}
+
+		n -= rv
+		pos += rv
+	}
+	return nil
 }
--- server/server.go
+++ server/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
 	"net"
 )
@@ -17,27 +19,91 @@ func main() {
 	for {
 		conn, err := socket.Accept()
 		if err != nil {
-			log.Fatal("Failed to accept connection: ", err)
+			log.Println("Failed to accept connection: ", err)
 			continue
 		}
 		defer conn.Close()
-		do_something(conn)
+
+		for {
+			e := one_request(conn)
+			if e != nil {
+				log.Println("error while processing requests: ", e)
+				break
+			}
+		}
 	}
 }
 
-func do_something(conn net.Conn) {
-	rbuf := make([]byte, 64)
-	_, err := conn.Read(rbuf)
-	if err != nil {
-		log.Fatal("Failed to read: ", err)
-		return
-	}
-	fmt.Println("client says: ", string(rbuf))
+var k_max_msg uint32 = 4096
 
-	wbuf := []byte("world")
-	_, err = conn.Write(wbuf)
+func one_request(conn net.Conn) error {
+	// 4 bytes header
+	rbuf := make([]byte, 4+k_max_msg+1)
+	err := read_full(conn, rbuf, 4)
 	if err != nil {
-		log.Fatal("Failed to write: ", err)
-		return
+		return err
 	}
+
+	msg_len := binary.LittleEndian.Uint32(rbuf[:4])
+
+	if msg_len > k_max_msg {
+		return fmt.Errorf("Message too long!")
+	}
+
+	// request body
+	err = read_full(conn, rbuf[4:], int(msg_len))
+	if err != nil {
+		return err
+	}
+
+	// do something
+	rbuf[4+msg_len] = '\x00'
+
+	fmt.Println("client says: ", string(rbuf[4:]))
+
+	// reply using same protocol
+	reply := []byte("world")
+	msg_len = uint32(len(reply))
+	wbuf := make([]byte, 4+msg_len)
+	binary.LittleEndian.PutUint32(wbuf, msg_len)
+	copy(wbuf[4:], reply)
+	return write_all(conn, wbuf, int(4+msg_len))
+}
+
+func read_full(c net.Conn, buf []byte, n int) error {
+	reader := io.LimitReader(c, int64(n))
+	pos := 0
+	for n > 0 {
+		rv, err := reader.Read(buf[pos:])
+		if err != nil {
+			return err
+		}
+
+		if rv > n {
+			panic("we have limited the reader, we should never reach here")
+		}
+
+		n -= rv
+		pos += rv
+	}
+
+	return nil
+}
+
+func write_all(c net.Conn, buf []byte, n int) error {
+	pos := 0
+	for n > 0 {
+		rv, err := c.Write(buf[pos:n])
+		if err != nil {
+			return err
+		}
+
+		if rv > n {
+			panic("we should never reach here")
+		}
+
+		n -= rv
+		pos += rv
+	}
+	return nil
 }
